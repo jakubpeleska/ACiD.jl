5322df82ed1debe2be17c13be2963d75652db699
Jakub Peleska <pelesjak@fel.cvut.cz>
2024-02-11 13:41:00 +0100

    WIP: Resnet on GPU

--- scripts/resnet_test.jl
+++ scripts/resnet_test.jl
@@ -12,9 +12,9 @@ using ProgressBars
 
 
 
-model = ResNet(18; pretrain = false, inchannels = 3, nclasses = 10)
+model = ResNet(18; pretrain = false, inchannels = 3, nclasses = 10) |> gpu
 
-optim = Flux.setup(Flux.Adam(), model)
+optim = Flux.setup(Flux.Adam(), model) |> gpu
 
 labels = collect(0:9)
 batchsize = 128
@@ -34,16 +34,19 @@ testY = Flux.onehotbatch(testSet.targets, labels)
 testLoader = DataLoader((testX, testY), batchsize = batchsize, shuffle = true)
 
 
-loss3(m, (x, y)) = Flux.logitcrossentropy(m(x), y)
+loss3(m, (x, y)) = Flux.logitcrossentropy(m(x), y) |> gpu
 
 for epoch in 1:100
     for d in ProgressBar(trainLoader)
+        d = d |> gpu
         ∂L∂m = Flux.gradient(loss3, model, d...)[1]
         Flux.update!(optim, model, ∂L∂m)
     end
 
     testLoss = 0
     for (x, y) in testLoader
+        x = x |> gpu
+        y = y |> gpu
         testLoss += Flux.logitcrossentropy(model(x), y, agg = sum)
     end
     println("Test Loss:", testLoss / length(testSet))
