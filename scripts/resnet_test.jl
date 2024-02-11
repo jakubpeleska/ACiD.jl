fc0b8d3c798b5a0afc4e2a229f0bba6ac44da890
Jakub Peleska <pelesjak@fel.cvut.cz>
2024-02-11 22:39:20 +0100

    Resnet on CPU

--- scripts/resnet_test.jl
+++ scripts/resnet_test.jl
@@ -8,13 +8,15 @@ using Optimisers
 
 using MLDatasets: CIFAR10
 
-using ProgressBars
+using ProgressBars: ProgressBar, update, set_description, set_postfix
+
+using Printf
 
 
 
-model = ResNet(18; pretrain = false, inchannels = 3, nclasses = 10) |> gpu
+model = ResNet(18; pretrain = false, inchannels = 3, nclasses = 10)
 
-optim = Flux.setup(Flux.Adam(), model) |> gpu
+optim = Flux.setup(Flux.Adam(), model)
 
 labels = collect(0:9)
 batchsize = 128
@@ -33,22 +35,48 @@ testX = testSet.features
 testY = Flux.onehotbatch(testSet.targets, labels)
 testLoader = DataLoader((testX, testY), batchsize = batchsize, shuffle = true)
 
+epochs = 100
+for e in 1:epochs
+    trainLoss = 0
+    trainedSamples = 0
+    trainIter = ProgressBar(trainLoader)
+    for (x, y) in trainIter
+        l, gs = Flux.withgradient(model) do m
+            Flux.logitcrossentropy(m(x), y, agg = sum)
+        end
+        Flux.update!(optim, model, gs[1])
 
-loss3(m, (x, y)) = Flux.logitcrossentropy(m(x), y) |> gpu
-
-for epoch in 1:100
-    for d in ProgressBar(trainLoader)
-        d = d |> gpu
-        ∂L∂m = Flux.gradient(loss3, model, d...)[1]
-        Flux.update!(optim, model, ∂L∂m)
+        trainedSamples += size(x, 4)
+        trainLoss += l
+        set_description(trainIter, @sprintf("Epoch %d/%d", e, epochs))
+        set_postfix(
+            trainIter,
+            train_loss = @sprintf("%.4f", trainLoss / trainedSamples)
+        )
     end
+    trainLoss /= trainedSamples
 
+    testSamples = 0
+    testAcc = 0
     testLoss = 0
     for (x, y) in testLoader
-        x = x |> gpu
-        y = y |> gpu
-        testLoss += Flux.logitcrossentropy(model(x), y, agg = sum)
+        testSamples += size(x, 4)
+        out = model(x)
+        ŷ = Flux.onecold(out, labels)
+        yₜ = Flux.onecold(y, labels)
+        testAcc += sum(ŷ .== yₜ)
+        testLoss += Flux.logitcrossentropy(out, y, agg = sum)
     end
-    println("Test Loss:", testLoss / length(testSet))
+
+    testAcc /= testSamples
+    testLoss /= testSamples
+
+    set_postfix(
+        trainIter,
+        train_loss = @sprintf("%.4f", trainLoss),
+        test_acc = @sprintf("%.4f", testAcc),
+        test_loss = @sprintf("%.4f", testLoss)
+    )
+    update(trainIter, 0, force_print = true)
 end
 
