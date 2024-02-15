cfe9cbf8086ca3af9d4b267dcfc0aa5395c503b6
Adrian <adrian.filcik@gmail.com>
2024-02-15 09:02:13 +0100

    Updated tests with better conditions

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,7 @@ include("resnet_test_minimal.jl")
 @testset "ACiD.jl" begin
     # train_loss: 1.4556, test_acc: 0.4329, test_loss: 1.8672
     train_loss, test_acc, test_loss = test_cifar_training()
-    @test ≈(train_loss, 1.4556, atol = 0.25) || train_loss <= 1.7056
-    @test ≈(test_acc, 0.4329, atol = 0.25) || test_acc <= 0.6829
-    @test ≈(test_loss, 1.8672, atol = 0.25) || test_loss <= 2.1172
+    @test train_loss <= 1.7056  # previously used ≈() function with atol=0.25
+    @test test_acc <= 0.6829
+    @test test_loss <= 2.1172
 end
